a94c3f556a615dd880558eea8682b089f429eb1b
ashepel <ashepel@usetech.ru>
2018-11-18 00:41:35 +0300

    Initial commit

---
+++ src/main/java/ru/usetech/tests/Square.java
@@ -0,0 +1,15 @@
+package ru.usetech.tests;
+
+public class Square {
+
+    public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+
+    public double area() {
+
+        return l * l;
+    }
+}
---
+++ src/test/java/ru/usetech/tests/SquareTests.java
@@ -0,0 +1,12 @@
+package ru.usetech.tests;
+
+public class SquareTests {
+    public void testArea() {
+        Square s = new Square(5.0);
+        assert s.area() == 25;
+
+        Rectangle r = new Rectangle(5.0, 6.0);
+        assert r.area() == 30;
+    }
+
+}
