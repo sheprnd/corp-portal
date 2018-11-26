76635fea960a0de47132f396838b7358ea153475
ashepel <ashepel@usetech.ru>
2018-11-26 22:34:36 +0300

    Refactoring

---
+++ src/test/java/ru/usetech/tests/Scroll.java
@@ -0,0 +1,19 @@
+package ru.usetech.tests;
+
+public class Scroll {
+    private final String start;
+    private final String finish;
+
+    public Scroll(String start, String finish) {
+        this.start = start;
+        this.finish = finish;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getFinish() {
+        return finish;
+    }
+}
