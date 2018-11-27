9a153f9d6cc8a26c1689446527179fe06b38e324
sheprnd <sheprnd@gmail.com>
2018-11-27 12:17:05 +0300

    added waiting to steps

--- src/test/java/ru/usetech/tests/ManualIncCreationTest.java
+++ src/test/java/ru/usetech/tests/ManualIncCreationTest.java
@@ -2,16 +2,24 @@ package ru.usetech.tests;
 
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class ManualIncCreationTest extends TestBase {
 
 
     @Test
     private void testBody() throws Exception {
         login("vm_user02@mail.ru", "12345");
+        TimeUnit.SECONDS.sleep(2);
         openModal();
+        TimeUnit.SECONDS.sleep(2);
         enterPostUrl(new EnterPostUrl("http://dwarfpool.com/xmr/"));
+        TimeUnit.SECONDS.sleep(2);
         enterAnswerContent("Текст ответа такой-то");
+        TimeUnit.SECONDS.sleep(2);
         selectLocation();
+        TimeUnit.SECONDS.sleep(2);
+        TimeUnit.SECONDS.sleep(2);
         pressCreateBtn();
 
 
--- src/test/java/ru/usetech/tests/UserCreationTest.java
+++ src/test/java/ru/usetech/tests/UserCreationTest.java
@@ -2,6 +2,8 @@ package ru.usetech.tests;
 
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class UserCreationTest extends TestBase{
 
 
@@ -9,9 +11,11 @@ public class UserCreationTest extends TestBase{
   @Test
   private void goToUsersPage() throws Exception {
     login("vm_user02@mail.ru", "12345");
+    TimeUnit.SECONDS.sleep(2);
     goToSettings();
+    TimeUnit.SECONDS.sleep(2);
     openCreationUserForm();
+    TimeUnit.SECONDS.sleep(2);
     newUserFormSubmission();
-
   }
 }
