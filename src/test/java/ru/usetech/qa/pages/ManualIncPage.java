34896da1c4e767adf5c52ddd1d62ca195825b455
sheprnd <sheprnd@gmail.com>
2019-01-10 16:20:00 +0300

    Create ManualIncPage.java

---
+++ src/test/java/ru/usetech/qa/pages/ManualIncPage.java
@@ -0,0 +1,38 @@
+package ru.usetech.qa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.stalenessOf;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
+public class ManualIncPage extends Page {
+
+    public ManualIncPage(WebDriver driver){
+        super(driver);
+        PageFactory.initElements(driver,this);
+
+    }
+    @FindBy(css=".btn-big")
+    private WebElement createManualIncidentHeaderBtn;
+
+    @FindBy(css="div.modal-footer > button.btn__blue")
+    private WebElement createManualIncidentFooter;
+
+    @FindBy(css="input.ng-invalid")
+    private WebElement postUrlField;
+
+    @FindBy(css=".textarea")
+    private WebElement textArea;
+
+
+
+}
