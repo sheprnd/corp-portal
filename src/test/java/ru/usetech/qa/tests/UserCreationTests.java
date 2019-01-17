45e1b256df246b4c26a836372b590811c305a86b
Anna Falileeva <laf2009@yandex.ru>
2019-01-17 18:29:55 +0300

    формирвоание рандомного мэйла для юзера

--- src/test/java/ru/usetech/qa/tests/UserCreationTests.java
+++ src/test/java/ru/usetech/qa/tests/UserCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.usetech.qa.model.UserData;
 
+import java.util.Random;
+
 import static org.testng.Assert.assertEquals;
 
 public class UserCreationTests extends TestBase{
@@ -12,15 +14,15 @@ public class UserCreationTests extends TestBase{
     public void ensurePreconditions(){
         app.goTo().settings();
         app.settings().goToUsers();
-
     }
 
     @Test
     public void testUserCreation() {
 
         int before = app.users().list();
-        app.users().create(new UserData().withLastName("тест_Фамилия").withFirstName("тест_Имя")
-                .withEmail("11111111111111@yandex.ru").withPassword("1")); //доделать рандомное получение данных юзера
+        app.users().create(new UserData().withLastName("#auto LastName").withFirstName("#auto FirstName")
+                .withEmail(new Random().nextInt(10000) + "@yandex.ru").withPassword("1")); //доделать рандомное получение данных юзера
+
         int after = app.users().list();
 
         assertEquals(before , after + 1);
