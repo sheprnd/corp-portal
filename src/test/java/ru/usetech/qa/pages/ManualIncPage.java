8def79cb70f0566a9ce3b2569165320b0b44b540
sheprnd <sheprnd@gmail.com>
2019-01-23 16:44:25 +0300

    Green alert message check added

--- src/test/java/ru/usetech/qa/pages/ManualIncPage.java
+++ src/test/java/ru/usetech/qa/pages/ManualIncPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.usetech.qa.model.ManIncData;
 
@@ -12,51 +11,62 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class ManualIncPage extends Page {
 
-  public ManualIncPage(WebDriver driver) {
-    super(driver);
-    PageFactory.initElements(driver, this);
+    public ManualIncPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
 
-  }
+    }
 
-  @FindBy(css = ".btn-big")
-  private WebElement createBtn;
+    @FindBy(css = ".btn-big")
+    private WebElement createBtn;
 
-  @FindBy(css = "div.modal-footer > button.btn__blue")
-  private WebElement saveBtn;
+    @FindBy(css = "div.modal-footer > button.btn__blue")
+    private WebElement saveBtn;
 
-  @FindBy(css = "input.ng-invalid")
-  private WebElement postUrl;
+    @FindBy(css = "[formcontrolname=newPostBlogUrl]")
+    private WebElement postUrl;
 
-  @FindBy(css = ".textarea")
-  private WebElement postText;
+    @FindBy(css = "[formcontrolname=newPostText]")
+    private WebElement postText;
 
-  @FindBy(css = ".modal-dialog")
-  private WebElement modalDialog;
+    @FindBy(css = ".modal-dialog")
+    private WebElement modalDialog;
 
-  /*@FindBy(css = "label.ng-tns-c0-8")
-  private WebElement locationSelector;*/
+    @FindBy(css = ".alert-success ")
+    private WebElement alertSuccess;
 
-  public void initManIncPage() {
-    wait.until(ExpectedConditions.elementToBeClickable(createBtn));
-    click(createBtn);
-    wait.until(visibilityOf(modalDialog));
+    @FindBy(css = ".ng-star-inserted[text='Ошибка содания инцидента']")
+    private WebElement alertNotSuccess;
 
-  }
+    public void initManIncPage() {
+        wait.until(ExpectedConditions.elementToBeClickable(createBtn));
+        click(createBtn);
+        wait.until(visibilityOf(modalDialog));
 
-  public void fillmanIncData(ManIncData manIncData) {
+    }
 
-    type(postUrl, manIncData.getPostUrlField());
-    type(postText, manIncData.getPostText());
-  }
+    public void fillmanIncData(ManIncData manIncData) {
 
-  public void clickSave() {
-    wait.until(ExpectedConditions.elementToBeClickable(saveBtn));
-    click(saveBtn);
-  }
+        type(postUrl, manIncData.getPostUrlField());
+        type(postText, manIncData.getPostText());
+    }
 
-  public void isManIncPageClosed(){
-      wait.until(ExpectedConditions.visibilityOf(modalDialog));
-  }
+    public void clickSave() {
+        wait.until(ExpectedConditions.elementToBeClickable(saveBtn));
+        click(saveBtn);
+    }
+
+    public void isManIncPageClosed() {
+        wait.until(ExpectedConditions.visibilityOf(modalDialog));
+    }
+
+    public void alertSuccess() {
+        wait.until(ExpectedConditions.visibilityOf(alertSuccess));
+    }
+
+    public void alertNotSuccess() {
+        wait.until(ExpectedConditions.visibilityOf(alertNotSuccess));
+    }
 
 
 }
--- src/test/java/ru/usetech/qa/tests/IncidentsTests.java
+++ src/test/java/ru/usetech/qa/tests/IncidentsTests.java
@@ -9,7 +9,13 @@ public class IncidentsTests extends TestBase {
         app.openManualincPage().initManIncPage();
         app.openManualincPage().fillmanIncData(new ManIncData().postText("#Random text").postUrlField("https://www.google.com/search/1"));
         app.openManualincPage().clickSave();
-        app.openManualincPage().isManIncPageClosed(); //Needs to redo based on pass alert message
+        app.openManualincPage().isManIncPageClosed();
+        try {
+            app.openManualincPage().alertSuccess();
+        } catch (Exception e){
+            System.out.println("Incident was not created error");
+        }
+
 
     }
 
