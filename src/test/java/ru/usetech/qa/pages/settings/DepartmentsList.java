5ae27fe6d979d6917c9b928b4eff53eca6f8efff
Anna Falileeva <laf2009@yandex.ru>
2019-03-19 13:38:29 +0300

    ApplicationManager структурирование, добавлены классы List, DepartmentsList, в тест создания отдела добавлена проверка на количество элементов в списке

---
+++ src/test/java/ru/usetech/qa/pages/settings/DepartmentsList.java
@@ -0,0 +1,30 @@
+package ru.usetech.qa.pages.settings;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import ru.usetech.qa.pages.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
+public class DepartmentsList extends List {
+
+    public DepartmentsList(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = ".categories > div.table__line:nth-child(2)")
+    private WebElement firstRow;
+
+    public int count() {
+
+        wait.until(visibilityOf(firstRow));
+        scrollList();
+        return driver.findElements(By.cssSelector(".categories > div.table__line")).size();
+
+    }
+
+}
