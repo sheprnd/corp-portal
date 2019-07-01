8882ec151a97bc826d5cdb1f00ceb670ee1951a4
Anna Falileeva <laf2009@yandex.ru>
2019-07-01 17:47:24 +0300

    для отладки 3

--- src/test/java/ru/usetech/qa/tests/SettingsTests.java
+++ src/test/java/ru/usetech/qa/tests/SettingsTests.java
@@ -90,7 +90,7 @@ public class SettingsTests extends TestBase {
         assertEquals(actualCount, count, "После удаления пользователя количество пользоватей в списке не уменьшилось на 1.");
         List<UserData> after = app.users().getList();
         assertEquals(new HashSet<>(after), new HashSet<>(before),  "Отличаются ожидаемый и полученный список пользователей после удаления пользователя.");
-    }
+    }*/
 
     @Test(priority=4)
     public void testDepartmentCreation() throws Exception{
@@ -119,7 +119,7 @@ public class SettingsTests extends TestBase {
         assertEquals(new HashSet<>(after), new HashSet<>(before),  "Отличаются ожидаемый и полученный список отделов после добавления нового отдела.");
     }
 
-    @Test(priority=5)
+   /* @Test(priority=5)
     public void testDepartmentEditing() throws Exception{
 
         app.settings().goToDepartments();
@@ -147,7 +147,7 @@ public class SettingsTests extends TestBase {
 
     }*/
 
-    @Test(priority=6, invocationCount = 10)
+    @Test(priority=6)
     public void testDepartmentDeletion() throws Exception {
 
         app.settings().goToDepartments();
